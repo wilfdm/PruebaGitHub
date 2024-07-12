15bb17ea33711687128ba44aa8734f2b5e03ab75
wilfdm <wilf.d.martinez@gmail.com>
2024-07-12 00:13:54 -0400

    Agregado el smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,18 @@
+
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#nosotros, #contactos, #destacados'
+  })
+
+
 
 $(document).ready(function() {
     $(".card-title").click(function() {
         $(this).siblings(".card-text").toggle();
     });
 });
+
